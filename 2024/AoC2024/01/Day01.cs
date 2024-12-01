760062d7c7a0ee5976677d2a93da287757e379fa
Fabian Birk <Fabian.Birk@medatixx.de>
2024-12-01 22:45:47 +0100

    AoC 2024 - Tag 1

---
+++ 2024/AoC2024/01/Day01.cs
@@ -0,0 +1,53 @@
+﻿namespace AoC2024._01
+{
+    public class Day01
+    {
+        public static int Part1()
+        {
+            var left = new List<int>();
+            var right = new List<int>();
+
+            var lines = File.ReadAllLines(@"01\data01.txt");
+
+            foreach (var line in lines)
+            {
+                var elements = line.Split(' ');
+                left.Add(int.Parse(elements[0]));
+                right.Add(int.Parse(elements[3]));
+            }
+
+            left.Sort();
+            right.Sort();
+
+            var diff = 0;
+            for (var i = 0; i < left.Count; i++)
+            {
+                diff += Math.Abs(right[i] - left[i]);
+            }
+
+            return diff;
+        }
+
+        public static int Part2()
+        {
+            var left = new List<int>();
+            var right = new int[100000];
+
+            var lines = File.ReadAllLines(@"01\data01.txt");
+            foreach (var line in lines) {
+                var elements = line.Split(' ');
+                left.Add(int.Parse(elements[0]));
+                right[int.Parse(elements[3])]++;
+            }
+
+            var similarityScore = 0;
+
+            for (var i = 0; i < left.Count; i++)
+            {
+                similarityScore += right[left[i]] * left[i];
+            }
+
+            return similarityScore;
+        }
+    }
+}
---
+++ 2024/AoC2024/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AoC2024._01;
+
+Console.WriteLine("*************** Day 10 ********************");
+Console.WriteLine($"Part 1: {Day01.Part1()}");
+Console.WriteLine($"Part 2: {Day01.Part2()}");
+Console.ReadKey();
