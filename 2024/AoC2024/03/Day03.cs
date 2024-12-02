a1dc7881be558e581cc1852e4819e3f94a4202f1
Fabian Birk <Fabian.Birk@medatixx.de>
2024-12-02 22:16:33 +0100

    Prepare Day 03

---
+++ 2024/AoC2024/03/Day03.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC2024._03
+{
+    public class Day03
+    {
+        public static int Part1()
+        {
+            var lines = File.ReadAllLines(@"03\data03.txt");
+
+            var safeReports = 0;
+
+            foreach (var line in lines)
+            {
+                var levels = line.Split(' ').Select(int.Parse).ToArray();
+                safeReports += ValidateLine(levels);
+            }
+
+            return safeReports;
+        }
+    }
+}
--- 2024/AoC2024/Program.cs
+++ 2024/AoC2024/Program.cs
@@ -1,6 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-using AoC2024._02;
+﻿using AoC2024._03;
 
-Console.WriteLine($"Part 1: {Day02.Part1()}");
-Console.WriteLine($"Part 2: {Day02.Part2()}");
+Console.WriteLine($"Part 1: {Day03.Part1()}");
+//Console.WriteLine($"Part 2: {Day03.Part2()}");
 Console.ReadKey();
