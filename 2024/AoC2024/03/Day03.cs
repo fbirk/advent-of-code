9f2ee2d972fd7b3e729a977243f84ac3811669a9
Fabian Birk <Fabian.Birk@medatixx.de>
2024-12-03 07:13:29 +0100

    AoC Day 03

--- 2024/AoC2024/03/Day03.cs
+++ 2024/AoC2024/03/Day03.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text.RegularExpressions;
 
 namespace AoC2024._03
 {
@@ -10,17 +6,77 @@ namespace AoC2024._03
     {
         public static int Part1()
         {
+            var multResult = 0;
             var lines = File.ReadAllLines(@"03\data03.txt");
-
-            var safeReports = 0;
 
             foreach (var line in lines)
             {
-                var levels = line.Split(' ').Select(int.Parse).ToArray();
-                safeReports += ValidateLine(levels);
+                var pattern = "(mul\\(\\d+,\\d+\\))";
+                var matches = Regex.Matches(line, pattern);
+                if (matches != null)
+                {
+                    foreach (var match in matches.Select(x => x.Value))
+                    {
+                        var res = TryGetValue(match);
+                        if (res != -1)
+                        {
+                            multResult += res;
+                        }
+                    }
+                }
             }
 
-            return safeReports;
+            return multResult;
+        }
+
+        public static int Part2()
+        {
+            var multResult = 0;
+            var lines = File.ReadAllLines(@"03\data03.txt");
+
+            var doIsActive = true;
+
+            foreach (var line in lines)
+            {
+                var pattern = "(mul\\(\\d+,\\d+\\))|(don't\\(\\))|(do\\(\\))";
+                var matches = Regex.Matches(line, pattern);
+                if (matches != null)
+                {
+                    foreach (var match in matches.Select(x => x.Value))
+                    {
+                        var res = TryGetValue(match);
+                        if (res != -1 && doIsActive)
+                        {
+                            multResult += res;
+                        }
+                        else if (match == "don't()")
+                        {
+                            doIsActive = false;
+                        }
+                        else if (match == "do()")
+                        {
+                            doIsActive = true;
+                        }
+                    }
+                }
+            }
+
+            return multResult;
+        }
+
+        private static int TryGetValue(string multResult)
+        {
+            var pattern = "(\\d+,\\d+)";
+            if (Regex.IsMatch(multResult, pattern))
+            {
+                var match = Regex.Match(multResult, pattern).Groups[1].Value;
+                var values = match.Split(',');
+                var a = int.Parse(values[0]);
+                var b = int.Parse(values[1]);
+                //Console.WriteLine($"{a} * {b}");
+                return a * b;
+            }
+            return -1;
         }
     }
 }
--- 2024/AoC2024/Program.cs
+++ 2024/AoC2024/Program.cs
@@ -1,5 +1,5 @@
 ﻿using AoC2024._03;
 
-Console.WriteLine($"Part 1: {Day03.Part1()}");
-//Console.WriteLine($"Part 2: {Day03.Part2()}");
+//Console.WriteLine($"Part 1: {Day03.Part1()}");
+Console.WriteLine($"Part 2: {Day03.Part2()}");
 Console.ReadKey();
